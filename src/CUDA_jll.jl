139ad0a4e14ef27af867963e6a26d05a867c59cb
Elliot Saba <staticfloat@gmail.com>
2019-10-04 17:07:13 -0400

    CUDA_jll build 10.1.243+0

---
+++ src/CUDA_jll.jl
@@ -0,0 +1,31 @@
+module CUDA_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("e9e359dc-d701-5aa8-82ae-09bbf812ea83"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "CUDA", artifacts_toml) for e in artifacts["CUDA"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load CUDA; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module CUDA_jll
