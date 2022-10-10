b0f6e7141d9023d65c001246228d44e0ee66e191
jlbuild <juliabuildbot@gmail.com>
2022-10-10 11:22:40 +0000

    CUDA_jll build 11.7.1+0

--- src/wrappers/x86_64-w64-mingw32.jl
+++ src/wrappers/x86_64-w64-mingw32.jl
@@ -8,7 +8,7 @@ JLLWrappers.@declare_file_product(libcudadevrt)
 JLLWrappers.@declare_library_product(libcudart, "cudart64_110.dll")
 JLLWrappers.@declare_library_product(libcufft, "cufft64_10.dll")
 JLLWrappers.@declare_library_product(libcufftw, "cufftw64_10.dll")
-JLLWrappers.@declare_library_product(libcupti, "cupti64_2022.2.0.dll")
+JLLWrappers.@declare_library_product(libcupti, "cupti64_2022.2.1.dll")
 JLLWrappers.@declare_library_product(libcurand, "curand64_10.dll")
 JLLWrappers.@declare_library_product(libcusolver, "cusolver64_11.dll")
 JLLWrappers.@declare_library_product(libcusolverMg, "cusolverMg64_11.dll")
@@ -69,7 +69,7 @@ function __init__()
 
     JLLWrappers.@init_library_product(
         libcupti,
-        "bin\\cupti64_2022.2.0.dll",
+        "bin\\cupti64_2022.2.1.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
